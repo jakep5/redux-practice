9ed1c7e4ca18377dc95f3736d531a08fb20bf347
jakep5 <jakepagel1@yahoo.com>
2020-06-15 13:09:43 -0500

    create app tree in app.js

--- src/index.js
+++ src/index.js
@@ -3,37 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
+import todoApp from './reducers';
 import { createStore } from 'redux';
 
-import todoApp from './reducers';
-import {
-  addTodo,
-  toggleTodo,
-  setVisibilityFilter,
-  VisibilityFilters
-} from './actions';
+const store = createStore(TodoApp)
 
-const store = createStore(todoApp, window.STATE_FROM_SERVER);
-
-console.log(store.getState());
-
-const unsubscribe = store.subscribe(() => console.log(store.getState()))
-
-store.dispatch(addTodo('Learn about actions'));
-store.dispatch(addTodo('Learn about reducers'));
-store.dispatch(addTodo('Learn about store'));
-store.dispatch(toggleTodo(0));
-store.dispatch(toggleTodo(1));
-store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED));
-
-unsubscribe();
-
-/* ReactDOM.render(
+ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
-); */
+);
 
